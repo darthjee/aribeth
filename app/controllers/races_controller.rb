368cdb42c6891cdd8afbf7a5826c51108a6f7a5d
Fernando Favini <darthjee@gmail.com>
2018-04-22 16:00:29 -0300

    Extract resource controller

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,26 +1,5 @@
-class RacesController < ApplicationController
+class RacesController < ResourceController
   include Common
-  # GET /races
-  # GET /races.json
-  alias_method :index, :render_basic
-
-  # GET /races/1
-  # GET /races/1.json
-  alias_method :show, :render_basic
-
-  # GET /races/new
-  alias_method :new, :render_basic
-
-  # GET /races/1/edit
-  alias_method :edit, :render_basic
-
-  # POST /races
-  # POST /races.json
-  alias_method :create, :render_basic
-
-  # PATCH/PUT /races/1
-  # PATCH/PUT /races/1.json
-  alias_method :update, :render_basic
 
   # DELETE /races/1
   # DELETE /races/1.json
