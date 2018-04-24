3446780d456eea8c46706cf6ec7d4ddfc2c2760b
Fernando Favini <darthjee@gmail.com>
2018-04-24 10:24:51 -0300

    Build controller

--- app/controllers/concerns/resourceable.rb
+++ app/controllers/concerns/resourceable.rb
@@ -2,6 +2,14 @@ module Resourceable
   extend ActiveSupport::Concern
   include Rendereable
 
+  class Builder
+    attr_reader :clazz
+
+    def initialize(clazz)
+      @clazz = clazz
+    end
+  end
+
   class_methods do
     def resource_for(name)
       %i(index show new edit create update).each do |method|
