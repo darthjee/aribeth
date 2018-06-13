1dc27dddf5716d3244b3d1d4bc1f737cdc2a8f3d
darthjee <darthjee@gmail.com>
2018-06-13 10:57:30 +0200

    Add resource methods

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -13,35 +13,7 @@ class RacesController < ApplicationController
 
   private
 
-  def new_resource
-    @new_resource ||= Race.new
-  end
-
-  def create_resource
-    @create_resource ||= Race.create(race_params)
-  end
-
-  def update_resource
-    @update_resource ||= race.tap { |v| v.update(race_params) }
-  end
-
-  def race
-    @race ||= races.find(race_id)
-  end
-
-  def races
-    @races ||= Race.all
-  end
-
-  def race_id
-    params.require(:id)
-  end
-
   def race_params
     params.require(:race).permit(:code, :playable)
   end
-
-  alias_method :index_resource, :races
-  alias_method :edit_resource, :race
-  alias_method :show_resource, :race
 end
